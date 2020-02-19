d2803d679d078a2f70a58bbd9ec90a0b00bb7124
Shruthi CS <shruthi.cs@aux.dkatalis.com>
2020-02-19 14:31:10 +0530

    changed login resposne

--- handlers/auth.js
+++ handlers/auth.js
@@ -4,10 +4,14 @@ const jwt = require('jsonwebtoken')
 
 const login = async (request, h) => {
   const userdetails = request.payload
-  return User.findOne({ email: userdetails.email, password: userdetails.password }).lean().then(loggedInUser => {
+  return User.findOne({ email: userdetails.email, password: userdetails.password }, { password: 0, _id: 0, __v: 0 }).lean().then(loggedInUser => {
     let response = unauthorizedResponse
+    console.log(loggedInUser)
     if (loggedInUser) {
-      response = { token: jwt.sign({ email: userdetails.email }, 'cartFuLL9876') }
+      const token = jwt.sign({ email: userdetails.email }, 'cartFuLL9876')
+      response = successResponse
+      loggedInUser.token = token
+      response.result = loggedInUser
     }
     return response
   })
